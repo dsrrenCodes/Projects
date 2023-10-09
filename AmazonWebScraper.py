f355a42d523c2ecb2038c391eb437dca5f71e653
dsrrenCodes <120300295+dsrrenCodes@users.noreply.github.com>
2023-10-09 17:57:06 +0800

    Update AmazonWebScraper.py

--- AmazonWebScraper.py
+++ AmazonWebScraper.py
@@ -1,33 +1,73 @@
 from bs4 import BeautifulSoup
 import requests
 
+def main():
+    
+    
+        #Amazon url playstation 5
+        url="https://www.amazon.com/s?k=playstation+5&crid=2U1A61UK7E9W2&sprefix=playstatio%2Caps%2C642&ref=nb_sb_ss_ts-doa-p_1_10"
 
-url="https://www.amazon.com/s?k=playstation+5&crid=2U1A61UK7E9W2&sprefix=playstatio%2Caps%2C642&ref=nb_sb_ss_ts-doa-p_1_10"
+        #headers to ensure web is recieved by genuine user, #insert ur own user agent
+        HEADERS=({"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0","Accept-Language":"en-US, en;q=0.5"})
 
-#headers to ensure web is recieved by genuine user
-HEADERS=({"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0","Accept-Language":"en-US, en;q=0.5"})
+        #http request 
+        req = requests.get(url,headers=HEADERS) 
 
-#http request 
-req = requests.get(url,headers=HEADERS) 
+        #converting req to html format  
+        doc=BeautifulSoup(req.content,"html.parser")
 
-#converting req to html format
-doc=BeautifulSoup(req.content,"html.parser")
+        #obtained links and storing it in a list, #listform
+        links=doc.find_all("a",class_="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")
 
-#obtained links and storing it in a list
-links=doc.find_all("a",class_="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")
-link=links[0].get("href")
+        linklists=[]
+        
+        hmap={"Title":[], "Prices":[], "Rating":[], "Stock":[]}
+        
+        #taking lists and storing into linklists variable
+        for link in links:
+            L=link.get("href")
+            linklists.append(L)
+            
+            
+        for link in linklists:
+            newurl="https://www.amazon.com"+link
+            newreq=requests.get(newurl,headers=HEADERS)
+            newdoc=BeautifulSoup(newreq.content,"html.parser")
+            hmap["Title"].append(get_title(newdoc))
+            hmap["Prices"].append(get_prices(newdoc))
+            hmap["Rating"].append(get_rating(newdoc))
+            #hmap["Stock"].append()
+        return hmap
 
-#product link
-productlist="https://amazon.com"+link
 
-#repeat
-newreq= requests.get(productlist,headers=HEADERS)
 
-newdoc=BeautifulSoup(newreq.content,"html.parser")
 
-productname=newdoc.find("span",{"id":"productTitle"},class_="a-size-large product-title-word-break").string.strip()
-productprice=newdoc.find("span",class_="a-offscreen").string
-productrating=newdoc.find("span",class_="a-icon-alt").string
-productstock=newdoc.find("span",class_="a-size-medium a-color-success").string
-print(productstock)
-
+def get_title(doc):
+    try:
+        title=doc.find("span",{"id":"productTitle"},class_="a-size-large product-title-word-break").string.strip()
+        return title
+    
+    #in the case where title cannot be found then return blank
+    except(AttributeError):
+        return ""
+    
+    
+def get_prices(doc):
+    try:
+        price=doc.find("span",class_="a-offscreen").string.strip()
+        
+        return price
+    
+    except(AttributeError):
+        return ""
+    
+def get_rating(doc):
+    try:
+        rating=doc.find("span",class_="a-icon-alt").string.strip()
+        
+        return rating
+    
+    except(AttributeError):
+        return ""
+            
+print(main())
