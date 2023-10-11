7f8c0adaa8275c0fef4b34caf6ed4ffb82c70e41
dsrrenCodes <120300295+dsrrenCodes@users.noreply.github.com>
2023-10-11 18:01:35 +0800

    Update AmazonWebScraper.py

--- AmazonWebScraper.py
+++ AmazonWebScraper.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 import requests
-
+#Using bs4 to obtain data from a given url and storing data in a dictionary. 
+#Using tabulate module to convert data in the dictionary into a table as output
+#get_title gets name of product
+#get_prices gets the price of the product
+#get_rating gets the rating of the product
+#get_stock gets the availability of the product
 def main():
     
     
-        #Amazon url playstation 5
-        url="https://www.amazon.com/s?k=playstation+5&crid=2U1A61UK7E9W2&sprefix=playstatio%2Caps%2C642&ref=nb_sb_ss_ts-doa-p_1_10"
+        #Insert own amazon link
+        url=""
 
         #headers to ensure web is recieved by genuine user, #insert ur own user agent
-        HEADERS=({"User-Agent":" ":"en-US, en;q=0.5"})
+        HEADERS=({"User-Agent":" ","Accept-Language":"en-US, en;q=0.5"})
 
         #http request 
         req = requests.get(url,headers=HEADERS) 
@@ -37,7 +43,10 @@ def main():
             hmap["Prices"].append(get_prices(newdoc))
             hmap["Rating"].append(get_rating(newdoc))
             hmap["Stock"].append(get_stock(newdoc))
-        return hmap
+        
+        print(tabulate(hmap,headers=["Title","Prices","Rating","Stock"],tablefmt="presto"))
+            
+        
 
 
 
@@ -46,41 +55,43 @@ def get_title(doc):
     try:
         title=doc.find("span",{"id":"productTitle"},class_="a-size-large product-title-word-break").string.strip()
         
-    
     #in the case where title cannot be found then return blank
     except(AttributeError):
-        title= ""
+        title=""
+    
     return title
     
     
 def get_prices(doc):
     try:
         price=doc.find("span",class_="a-offscreen").string.strip()
-        
-        
-    
+
+    #in the case where price cannot be found then return blank
     except(AttributeError):
-        price= ""
+        price=""
     return price
+
     
 def get_rating(doc):
     try:
         rating=doc.find("span",class_="a-icon-alt").string.strip()
 
+    #in the case where rating cannot be found then return blank
     except(AttributeError):
         rating=""
-        
+    
     return rating
 
 def get_stock(doc):
-    try:
+    try:    
         stock=doc.find("span",class_="a-size-medium a-color-success").string.strip()
-        
+
+    #in the case where availability of stock cannot be found then return blank
     except(AttributeError):
         stock="No Stock"
     
     return stock
-    
 
-            
-print(main())
+    
+if __name__=="__main__":
+    main()
